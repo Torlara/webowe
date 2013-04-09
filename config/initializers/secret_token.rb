051cf519111a941d49311a8ca43015fcface1bbd
Milka <milkagaca@poczta.onet.pl>
2013-04-09 12:52:41 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webowe::Application.config.secret_token = 'd3af5738d2d42fe4c17e5c608d35e9276ff84f186c44fd1cb1f34c5a77b3be324b847c1f2554521beb3c4bde202b20a2132cdc791a7ae25c98dbea5e55d3cfa8'
